f3d5669262d2f6dfaecb25b993d5cbbfefdad046
Alex Lane <awlane@memphis.edu>
2020-10-08 20:38:07 -0500

    Working utility methods

--- rameniaapp/models/edit.py
+++ rameniaapp/models/edit.py
@@ -4,5 +4,5 @@ from django.conf import settings
 class Edit(models.Model):
     image = models.ImageField(blank=True)
     change = models.JSONField(null=True, blank=True)
-    noodle = models.ForeignKey("rameniaapp.Noodle", blank=True, on_delete=models.CASCADE)
+    noodle = models.ForeignKey("rameniaapp.Noodle", null=True, blank=True, on_delete=models.CASCADE)
     editor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- rameniaapp/views/noodle.py
+++ rameniaapp/views/noodle.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.conf import settings
 from django.db.models import Avg
 from rameniaapp.models import Noodle
+from .edit_util import apply_change
 
 def view_noodle(request, noodle_id):
     noodle = Noodle.objects.get(pk=noodle_id)
@@ -10,4 +11,11 @@ def view_noodle(request, noodle_id):
     template = loader.get_template('ramen.html')
     context = { "noodle" : noodle, "MEDIA_URL" : settings.MEDIA_URL,
                 "avg_rating" : avg_rating }
+    if len(noodle.edit_set.all()) > 0:
+        for edit in noodle.edit_set.all():
+            apply_change(edit)
+    from rameniaapp.models import Edit
+    if len(Edit.objects.filter(noodle=None)) > 0:
+        for edit in Edit.objects.filter(noodle=None):
+            apply_change(edit)
     return HttpResponse(template.render(context, request))

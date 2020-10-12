782b2c8cff5a0ed13f8179a92adc0cbe70200653
MikesBytes <mail@mikesbytes.org>
2020-10-11 19:19:43 -0500

    basic list styling

--- rameniaapp/views/list.py
+++ rameniaapp/views/list.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, HttpResponse
 from django.template import loader
 from django.conf import settings
+from django.db.models import Avg
 from rameniaapp.models import Noodle, NoodleImage
 
 def view_list(request, list_id):
     noodles = Noodle.objects.all()
     images = []
+    review_avgs = []
+    
     for noodle in noodles:
         image = NoodleImage.objects.filter(noodle__pk=noodle.id)[0]
+        avg_rating = noodle.review_set.all().aggregate(Avg('rating'))["rating__avg"]
         images.append(image)
-    print(images)
+        review_avgs.append(avg_rating)
+        
     template = loader.get_template('list.html')
-    context = { "noodles" : zip(noodles, images), "MEDIA_URL" : settings.MEDIA_URL }
+    context = { "noodles" : zip(noodles, images, review_avgs), "MEDIA_URL" : settings.MEDIA_URL }
     return HttpResponse(template.render(context, request))

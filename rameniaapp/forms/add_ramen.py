f8a3290a1b396dde5e9d8468dfc6f560fef3e1de
Eriel Traywick <ltrywick@memphis.com>
2020-10-14 19:22:56 -0500

    Old stuff

--- rameniaapp/forms/add_ramen.py
+++ rameniaapp/forms/add_ramen.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import Textarea
 from rameniaapp.models import Ramen
+from rameniaapp.models import Tag
 
 class AddRamenForm(forms.ModelForm):
     class Meta:
--- rameniaapp/models/add_ramen.py
+++ rameniaapp/models/add_ramen.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.conf import settings
 
 
+
 class Ramen(models.Model):
     name = models.CharField(max_length=100)
     manufacturer = models.CharField(max_length=100)
     description = models.TextField(max_length=400)
+   

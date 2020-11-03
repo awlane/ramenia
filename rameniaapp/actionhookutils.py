b60725650c51738b091f40591b76dd322704e5d8
MikesBytes <mail@mikesbytes.org>
2020-11-03 14:05:31 -0600

    params for action hooks

--- rameniaapp/actionhookutils.py
+++ rameniaapp/actionhookutils.py
@@ -1,21 +1,32 @@
 from rameniaapp.models import ActionHook, Badge
 from django.contrib.auth.models import User
 
-def dispatch_hook(user, hook_name):
+def dispatch_hook(user, hook_name, **kwargs):
     hooks = ActionHook.objects.filter(hook_name=hook_name)
     for hook in hooks:
-        if hook_name == "rep-changed":
-            if "lt" in hook.params:
-                if hook.params["target-val"] < user.metadata["Reputation"]:
-                    dispatch_hook(user, hook.params["lt"])
-            if "gteq" in hook.params:
-                if hook.params["target-val"] >= user.metadata["Reputation"]:
-                    dispatch_hook(user, hook.params["gteq"])
         if "change-rep" in hook.params:
             cur_rep =  user.profile.metadata["Reputation"]
             user.profile.metadata["Reputation"] = cur_rep + hook.params["change-rep"]
             user.profile.save()
+            dispatch_hook(user, "reputation", reputation=user.profile.metadata["Reputation"])
         if "add-badge" in hook.params:
             user.profile.badges.add(Badge.objects.get(pk=hook.params["add-badge"]))
         if "remove-badge" in hook.params:
             user.profile.badges.remove(Badge.objects.get(pk=hook.params["remove-badge"]))
+        for key, value in kwargs.items():
+            keyname = key + "-eq"
+            if keyname in hook.params and hook.params[keyname] == value:
+                dispatch_hook(hook.params[keyname])
+            keyname = key + "-lt"
+            if keyname in hook.params and hook.params[keyname] < value:
+                dispatch_hook(hook.params[keyname])
+            keyname = key + "-gt"
+            if keyname in hook.params and hook.params[keyname] > value:
+                dispatch_hook(hook.params[keyname])
+            keyname = key + "-lte"
+            if keyname in hook.params and hook.params[keyname] <= value:
+                dispatch_hook(hook.params[keyname])
+            keyname = key + "-gte"
+            if keyname in hook.params and hook.params[keyname] >= value:
+                dispatch_hook(hook.params[keyname])
+    

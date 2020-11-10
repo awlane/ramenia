bbb84c99825cf73b9aaca10b1461253c0e827bb0
Alex Lane <awlane@memphis.edu>
2020-11-10 15:40:00 -0600

    Add flash in page

--- rameniaapp/views/views.py
+++ rameniaapp/views/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
+    messages.add_message(request, messages.SUCCESS, 'Hello world.')
     return render(request, 'index.html')
 
 

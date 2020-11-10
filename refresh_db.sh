b3c1b58b1d0dd0a2f82c14a1522bb31fac2deaff
Alex Lane <awlane@memphis.edu>
2020-11-10 16:35:39 -0600

    More powerful refresh DB script and less breakable fixture

--- refresh_db.sh
+++ refresh_db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python3 manage.py flush
+echo 'drop owned by ramenia' | python3 manage.py dbshell
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py loaddata dbseed.json
